3c730d58d1ab8bb2dd991b0af60abf39fe637d60
Il Drago D'oro <vicen333@gmail.com>
2021-03-07 13:03:24 +0100

    Elimino index.md e inserto tarjetas de GitHub en el apartado correspondiente

--- views/git.php
+++ views/git.php
@@ -1,11 +1,24 @@
     <title>GitHub - Il Drago D'oro</title>
     <?php include('../common/header.php'); ?>
     <div class="container">
-      <div class="row">
-        <div class="col-12 text-center">
-          <iframe style="height:100%; width:100%;" src="https://ildragodoro.github.io/DragonWeb/.">
-            <b>IFrame is unavailable here</b>
-          </iframe>
+      <div class="row mt-3">
+        <div class="col-md-12 text-center">
+          <div class="github-card" data-github="IlDragoDoro" data-width="100%" data-height="auto;" data-theme="default"></div>
+          <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+        </div>
+      </div>
+      <hr>
+      <div class="row mt-3">
+        <div class="col-md-12 text-center">
+          <div class="github-card" data-github="IlDragoDoro/DragonWeb" data-width="100%" data-height="auto" data-theme="default"></div>
+          <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+        </div>
+      </div>
+      <hr>
+      <div class="row mt-3">
+        <div class="col-md-12 text-center">
+          <div class="github-card" data-github="IlDragoDoro/AtomClass" data-width="100%" data-height="" data-theme="default"></div>
+          <script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
         </div>
       </div>
     </div>
