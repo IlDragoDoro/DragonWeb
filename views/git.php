6ff070fb529b1626b1fe2357ec021103c7ddc190
Il Drago D'oro <vicen333@gmail.com>
2021-03-07 12:47:11 +0100

    ok

--- views/git.php
+++ views/git.php
@@ -1,7 +1,12 @@
     <title>GitHub - Il Drago D'oro</title>
     <?php include('../common/header.php'); ?>
-      <iframe style="height:300px; width:300px;" src="https://ildragodoro.github.io/DragonWeb/.">
-        <b>IFrame is unavailable here</b>
-</iframe>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <iframe style="height:100%; width:100%;" src="https://ildragodoro.github.io/DragonWeb/.">
+            <b>IFrame is unavailable here</b>
+          </iframe>
+        </div>
+      </div>
     </div>
     <?php include('../common/footer.php'); ?>
