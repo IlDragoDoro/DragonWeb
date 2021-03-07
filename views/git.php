2e07be0978f556165ee67010fc875b1b6b852ad1
Il Drago D'oro <vicen333@gmail.com>
2021-03-07 12:48:22 +0100

    creo el index.md para github web

--- views/git.php
+++ views/git.php
@@ -1,6 +1,12 @@
     <title>GitHub - Il Drago D'oro</title>
     <?php include('../common/header.php'); ?>
-    <div class="">
-
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <iframe style="height:100%; width:100%;" src="https://ildragodoro.github.io/DragonWeb/.">
+            <b>IFrame is unavailable here</b>
+          </iframe>
+        </div>
+      </div>
     </div>
     <?php include('../common/footer.php'); ?>
