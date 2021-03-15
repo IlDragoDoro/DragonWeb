5fb6eafbd6984a3e3cf132246770b60c33056a10
Il Drago D'oro <vicen333@gmail.com>
2021-03-15 13:14:32 +0100

    He hecho la parte sobre mi aunque la mejoraré!

--- views/about.php
+++ views/about.php
@@ -1,6 +1,106 @@
     <title>Sobre Nosotros - Il Drago D'oro</title>
     <?php include('../common/header.php'); ?>
-    <div class="">
+    <style media="screen">
+      h3{
+        font-family: cursive;
+      }
+      h4{
+        font-family: cursive;
+      }
+    </style>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-12">
+          <h3>Un poco de todo sobre mi:</h3>
+        </div>
+      </div>
 
+      <div class="row border mt-3 text-center">
+        <div class="col-md-4 border">
+          <h4>Nombre <i class="far fa-arrow-alt-circle-right"></i></h4>
+        </div>
+        <div class="col-md-8">
+          <h4><i class="fas fa-signature"></i> Vicenç</h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3 text-center">
+        <div class="col-md-8 border">
+          <h4><i class="fas fa-scroll"></i> Beltrán Díaz</h4>
+        </div>
+        <div class="col-md-4">
+          <h4><i class="far fa-arrow-alt-circle-left"></i>Apellidos</h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3 text-center">
+        <div class="col-md-4 border">
+          <h4 class="mt-2">Fecha de nacimiento <i class="far fa-arrow-alt-circle-right"></i></h4>
+        </div>
+        <div class="col-md-8">
+          <h4 class="mt-3"> <i class="fas fa-baby"></i> 21/04/1997 ( <label id="edad"></label> años ) </h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3 text-center">
+        <div class="col-md-8 border">
+          <h4><i class="fas fa-laptop-code"></i> FPGS Desarrollo de aplicaciones web.</h4>
+          <h4><i class="fas fa-cocktail"></i> Cocina y Restauración EHIB.</h4>
+          <h4><i class="fas fa-hotel"></i> FPGM Cocina y gastronomía.</h4>
+        </div>
+        <div class="col-md-4 mt-4">
+          <h4 ><i class="far fa-arrow-alt-circle-left"></i> Estudios</h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3 text-center">
+        <div class="col-md-4 border">
+          <h4 class="mt-5">Experiencia Laboral <i class="far fa-arrow-alt-circle-right"></i></h4>
+        </div>
+        <div class="col-md-8">
+          <h4 class="mt-3">2013-2018:</h4>
+          <h4> Ayudante de cocina en Mallorca, Bilbao y EEUU.</h4>
+          <h4> Comercial en el corte inglés en Bilbao.</h4>
+          <h4> Encuestador de Cambridge en Bilbao.</h4>
+          <h4> Comercial a pie de calle en Bilbao.</h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3 text-center">
+        <div class="col-md-8 border">
+          <h4> <i class="far fa-flag"></i> Español (Nativo)</h4>
+          <h4> <i class="fas fa-flag"></i> Catalán (Nativo)</h4>
+          <h4> <i class="fas fa-flag-usa"></i> Inglés (Intermedio - B1)</h4>
+          <small>[Titulación de EOI y trinity College London + Diploma de campus en  irlanda.]</small>
+        </div>
+        <div class="col-md-4 mt-4">
+          <h4 ><i class="far fa-arrow-alt-circle-left"></i> idiomas</h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3">
+        <div class="col-md-4 mt-4 text-center">
+          <h4 > Otros <i class="far fa-arrow-alt-circle-right"></i></h4>
+        </div>
+        <div class="col-md-8 border">
+          <h4><i class="fas fa-crosshairs"></i> Gran capacidad de aprendizaje</h4>
+          <h4><i class="fas fa-crosshairs"></i> Flexibilidad y capacidad de adaptación</h4>
+          <h4><i class="fas fa-crosshairs"></i> Disponibilidad horaria completa</h4>
+          <h4><i class="fas fa-crosshairs"></i> Permiso de conducir y vehiculo propio</h4>
+        </div>
+      </div>
+
+      <div class="row border mt-3">
+        <div class="col-md-12 text-center">
+          <h4 style="text-decoration: underline">Curiosidades:</h4>
+          <h4> <i class="fas fa-leaf"></i> Voluntário de Greenpeace y del GOB.</h4>
+          <h4> <i class="fas fa-plane"></i> Me encanta viajar.</h4>
+          <h4> <i class="fas fa-user-friends"></i> Soy muy sociable.</h4>
+          <h4> <i class="fab fa-gratipay"></i> Soy una persona leal y desinteresada.</h4>
+          <h4> <i class="fas fa-tasks"></i> Soy multitarea, me gusta hacer de todo.</h4>
+          <h4> <i class="fas fa-paint-brush"></i> Tengo una gran imaginación.</h4>
+        </div>
+      </div>
     </div>
+    <hr style="margin-top: 5%">
     <?php include('../common/footer.php'); ?>
